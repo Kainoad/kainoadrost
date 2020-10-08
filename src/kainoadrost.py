73c80c555d15fac557fe4a0f3a465129ff3ae922
Kainoa Drost <developer2334@gmail.com>
2020-10-08 14:49:00 -0400

    Implemented templates.

--- src/kainoadrost.py
+++ src/kainoadrost.py
@@ -1,16 +1,20 @@
 import subprocess
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-	return "<h1 style='color:blue'>Hello!</h1>"
+def index():
+    return render_template('index.html')
 
-@app.route("/git-pull")
+@app.route("/git-pull", methods=['GET', 'POST'])
 def git_pull():
     result = subprocess.run(['./scripts/git_fetch_pull.sh'], stdout=subprocess.PIPE)
+    f = open("test.txt", "a")
+    f.write(str(request.get_data()))
+    f.close()
+
     return "<h1 style='color:blue'>" + str(result.stdout.decode('utf-8')) + "</h1>"
 
 if __name__ == "__main__":
