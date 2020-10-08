62a7d61c249eb7702a622e9ba94e5d0fc5a1a84f
Kainoa Drost <developer2334@gmail.com>
2020-10-08 13:34:11 -0400

    Clean up and organize directory structure.

--- src/kainoadrost.py
+++ src/kainoadrost.py
@@ -10,7 +10,7 @@ def hello():
 
 @app.route("/git-pull")
 def git_pull():
-    result = subprocess.run(['./git_fetch_pull.sh'], stdout=subprocess.PIPE)
+    result = subprocess.run(['./scripts/git_fetch_pull.sh'], stdout=subprocess.PIPE)
     return "<h1 style='color:blue'>" + str(result.stdout.decode('utf-8')) + "</h1>"
 
 if __name__ == "__main__":
--- src/scripts/git_fetch_pull.sh
+++ src/scripts/git_fetch_pull.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+cd ..
 git fetch
 git reset --hard origin/master
-chmod +x git_fetch_pull.sh
+chmod +x src/scripts/git_fetch_pull.sh
 systemctl restart kainoadrost
